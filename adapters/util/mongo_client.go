c657918d30ca7c9ba98dc02b94980a9aad0fca60
HoroscopeTt <borwornkijsomboon@gmail.com>
2025-06-23 23:18:21 +0700

    Connet Mongo

--- adapters/util/mongo_client.go
+++ adapters/util/mongo_client.go
@@ -1,1 +1,36 @@
 package util
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+// InitMongoDBClient สร้างและคืนค่า MongoDB client
+func InitMongoDBClient(mongoURI string, timeout int) (*mongo.Client, error) {
+	client, err := mongo.NewClient(options.Client().ApplyURI(mongoURI))
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(timeout)*time.Second)
+	defer cancel()
+
+	err = client.Connect(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	// Ping the primary to verify connection
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		return nil, err
+	}
+
+	log.Println("Successfully connected to MongoDB!")
+	return client, nil
+}
--- main.go
+++ main.go
@@ -1,20 +1,33 @@
 package main
 
 import (
+	"context"
 	"log"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/horoscope/go-hexagonal-template/adapters/util"
 	"github.com/horoscope/go-hexagonal-template/config"
 )
 
 func main() {
 	cfg := config.LoadConfig()
 
+	mongoClient, err := util.InitMongoDBClient(cfg.MongoDBURI, cfg.MongoDBTimeout)
+	if err != nil {
+		log.Fatalf("Failed to initialize MongoDB client: %v", err)
+	}
+	defer func() {
+		if err = mongoClient.Disconnect(context.Background()); err != nil {
+			log.Printf("Error disconnecting from MongoDB: %v", err)
+		} else {
+			log.Println("Disconnected from MongoDB.")
+		}
+	}()
+
 	app := fiber.New()
 	app.Use(logger.New())
 
 	log.Printf("Bookstore API Service started on :%s", cfg.ServerPort)
 	log.Fatal(app.Listen(":" + cfg.ServerPort))
-
 }
